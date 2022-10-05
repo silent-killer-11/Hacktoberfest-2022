0115125a867944cbde077477ca4de5f79d6496be
Om Nalamwar <78678547+omnalamwar@users.noreply.github.com>
2022-10-05 23:26:04 +0530

    added some codes in cpp

---
+++ string.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string name, surname;
+
+    cin>>name;
+    cin>>surname;
+
+    string fullname = name + " " + surname;
+    cout<<"Fullname is: "<<fullname;
+    return 0;
+}
