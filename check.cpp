1580c9480435b60f54529533e9fab4d5ad03bd12
RudraNarayanHota-143 <89252181+RudraNarayanHota-143@users.noreply.github.com>
2022-10-06 02:53:32 +0530

    Update check.cpp

--- check.cpp
+++ check.cpp
@@ -1,29 +1,17 @@
-#include <bits/stdc++.h>
+/*  C Program to find area and circumference of circle  */
 
-using namespace std;
+#include<stdio.h>
 
+int main()
+{
+    float area, radius, circum;
 
+    printf("ENTER THE RADIUS OF THE CIRCLE :: ");
+    scanf("%f", &radius);
+    area = 3.142 * radius * radius;
+    circum = 2 * 3.142 * radius;
+    printf("\nTHE AREA OF THE CIRCLE IS :: %f", area);
+    printf("\n\nTHE CIRCUMFERENCE OF THE CIRCLE IS :: %f\n", circum);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-int main() {
-
-
-
-
-
-
-	return 0;
+    return 0;
 }
