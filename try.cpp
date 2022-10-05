b5153607fe6fcd249d4268ab0862f309ef5b9542
RudraNarayanHota-143 <89252181+RudraNarayanHota-143@users.noreply.github.com>
2022-10-06 02:50:33 +0530

    Update try.cpp

--- try.cpp
+++ try.cpp
@@ -1,23 +1,36 @@
-#include <bits/stdc++.h>
+/*  C Program to find the roots of quadratic equation  */
 
-using namespace std;
+#include<stdio.h>
+#include<math.h>
 
+int main()
+{
+    int A, B, C;
+    float disc, deno, x1, x2;
+    printf("ENTER THE VALUE OF A :: ");
+    scanf("%d", &A);
+    printf("\nENTER THE VALUE OF B :: ");
+    scanf("%d",&B);
+    printf("\nENTER THE VALUE OF C :: ");
+    scanf("%d",&C);
 
+    disc=(B*B)-(4*A*C);
+    deno = 2 * A;
+    if(disc > 0)
+    {
+    printf("\nTHE ROOTS ARE REAL ROOTS.");
+    x1 = (-B/deno)+(sqrt(disc)/deno);
+    x2 = (-B/deno)-(sqrt(disc)/deno);
+    printf("\n\nTHE ROOTS ARE :: %f and %f\n", x1, x2);
+    }
+    else if(disc == 0)
+    {
+    printf("\nTHE ROOTS ARE REPEATED ROOTS.");
+    x1 = -B/deno;
+    printf("\n\nTHE ROOT IS :: %f\n", x1);
+    }
+    else
+    printf("\nTHE ROOTS ARE IMAGINARY ROOTS.\n");
 
-
-
-
-
-
-
-
-
-int main() {
-
-
-
-
-
-
-	return 0;
+    return 0;
 }
