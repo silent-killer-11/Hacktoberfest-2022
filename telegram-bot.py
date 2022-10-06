7ffccb78aa030ad7f198c580c771c6c0bbeb3581
Subham <subham.rouniyar27@gmail.com>
2022-10-06 15:08:58 +0545

    added telegram bot

---
+++ telegram-bot.py
@@ -0,0 +1,64 @@
+#pip install pytelebotapi
+import telebot
+import yfinance as yf
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.environ.get("TOKEN")
+bot=telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['Greet'])
+def greet(message):
+    bot.reply_to(message," Hey, Hi you.")
+
+@bot.message_handler(commands=['stonks'])
+def get_stocks(message):
+  response = ""
+  stocks = ['gme', 'amc', 'nok']
+  stock_data = []
+  for stock in stocks:
+    data = yf.download(tickers=stock, period='2d', interval='1d')
+    data = data.reset_index()
+    response += f"-----{stock}-----\n"
+    stock_data.append([stock])
+    columns = ['stock']
+    for index, row in data.iterrows():
+      stock_position = len(stock_data) - 1
+      price = round(row['Close'], 2)
+      format_date = row['Date'].strftime('%m/%d')
+      response += f"{format_date}: {price}\n"
+      stock_data[stock_position].append(price)
+      columns.append(format_date)
+    print()
+
+  response = f"{columns[0] : <10}{columns[1] : ^10}{columns[2] : >10}\n"
+  for row in stock_data:
+    response += f"{row[0] : <10}{row[1] : ^10}{row[2] : >10}\n"
+  response += "\nStock Data"
+  print(response)
+  bot.send_message(message.chat.id, response)
+
+def stock_request(message):
+  request = message.text.split()
+  if len(request) < 2 or request[0].lower() not in "price":
+    return False
+  else:
+    return True
+
+@bot.message_handler(func=stock_request)
+def send_price(message):
+  request = message.text.split()[1]
+  data = yf.download(tickers=request, period='5m', interval='1m')
+  if data.size > 0:
+    data = data.reset_index()
+    data["format_date"] = data['Datetime'].dt.strftime('%m/%d %I:%M %p')
+    data.set_index('format_date', inplace=True)
+    print(data.to_string())
+    bot.send_message(message.chat.id, data['Close'].to_string(header=False))
+  else:
+    bot.send_message(message.chat.id, "No data!?")
+
+    
+
+bot.polling()
