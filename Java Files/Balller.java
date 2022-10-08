f4324fd09b5e0e965e539bbaedc2c247da22cad0
Riyajain115 <jainriya1528@gmail.com>
2022-10-09 00:47:51 +0530

    Java Files Added

---
+++ Java Files/Balller.java
@@ -0,0 +1,13 @@
+class Baller extends Cricketer { 
+    int ballers;
+    String tob;
+    Baller(int ballers, String tob)
+    {
+        this.ballers=ballers;
+        this.tob=tob;
+    }
+    void Message()
+    {
+        System.out.println("Hello I am a Baller");
+    }
+}
