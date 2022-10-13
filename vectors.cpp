d82f9a3b5c7025a589a0e06b17b0ac6634c02209
Mean M0nst3r <70705072+ghostgamer001@users.noreply.github.com>
2022-10-13 12:00:52 +0530

    Update vectors.cpp
    Implemented accessing elements in vector

--- vectors.cpp
+++ vectors.cpp
@@ -16,5 +16,20 @@ int main(){
     for(auto x: v){
         cout<<x<<" ";
     }
+    for (int i = 1; i <num; i++)
+        v.push_back(i * 10);
+ 
+    cout << "\nReference operator [v] : v[2] = " << v[2];
+ 
+    cout << "\nat : v.at(4) = " << v.at(3);
+ 
+    cout << "\nfront() : v.front() = " << v.front();
+ 
+    
+ 
+    // pointer to the first element
+    int* pos = v.data();
+ 
+    cout << "\nThe first element is " << *pos;
     return 0;
 }
